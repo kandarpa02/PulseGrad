3f9fd9fefc39a22108fbdee8bc82267416ddb01c
kandarpa02 <kandarpaexe@gmail.com>
2025-04-30 00:48:17 +0530

    fix: broadcasting issue

--- pulsar/core.py
+++ pulsar/core.py
@@ -94,7 +94,7 @@ class pulse:
             raise ValueError(f"Uhh Vibes didn't match!! :( please check the dimensions ( ,{k}) != ({k_}, )")
         
         result = np.dot(self.data, other.data)
-        out = pulse(result, (self, other), '@', compute_grad=True if self.compute_grad else False, size=(m, n))
+        out = pulse(result, (self, other), '@', compute_grad=True if self.compute_grad == True else False, size=(m, n))
         
         def _back():
             if  self.compute_grad == True:
