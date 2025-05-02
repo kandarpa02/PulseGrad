06cb270fa5f59fe0a527f842bd44e1b34432e2ca
kandarpa02 <kandarpaexe@gmail.com>
2025-05-02 23:51:46 +0530

    add: gpu acceleration

--- setup.py
+++ setup.py
@@ -1,4 +1,10 @@
 import setuptools
+import platform
+
+cupy_version = "cupy>=12.0.0" 
+
+if platform.system() == "Linux":
+    cupy_version = "cupy>=12.0.0"
 
 setuptools.setup(
     name="PulseGrad",
@@ -16,6 +22,6 @@ setuptools.setup(
     python_requires='>=3.6',
     install_requires=[
         "numpy>=1.18.0",
-        "cupy>=1.18.0"
+        cupy_version  
     ],
 )
